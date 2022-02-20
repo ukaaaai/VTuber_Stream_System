7ae27f17219b0f7dfebeb61f441696a584a82e27
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-02-20 19:23:09 +0900

    add: gamemanager

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static string username = "1";
+    
+    public static string getUsername() => username;
+}
