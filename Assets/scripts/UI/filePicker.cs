1dc918fa6c6a24444659d79f6501f53fa968b41a
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-03-05 20:38:40 +0900

    update: using unity event

--- Assets/scripts/UI/filePicker.cs
+++ Assets/scripts/UI/filePicker.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 using System;
+using UnityEngine.Events;
 
 public class filePicker : MonoBehaviour
 {
+    [Serializable] class myEvent : UnityEvent<string> { }
+
+    [SerializeField] myEvent getPath = new myEvent();
+    
     public void OnClick()
     {
         try
         {
             string path = FilePick.getInstance().filePick();
             Debug.Log(path);
-            model_manager.AddModel(path);
+            getPath.Invoke(path);
         }
         catch (Exception)
         {
