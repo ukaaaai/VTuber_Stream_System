638b27351ef308999c6d0dd5ec98f3f985cb045b
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-06-20 12:47:14 +0900

    update: model path

--- Assets/scripts/managers/model_manager.cs
+++ Assets/scripts/managers/model_manager.cs
@@ -11,8 +11,18 @@ public class model_manager : MonoBehaviour
     private static Dictionary<string, CubismModel3Json> models = new Dictionary<string,CubismModel3Json>();
     private static Dictionary<string, CubismModel> objects = new Dictionary<string, CubismModel>();
 
-    //Singleton
-    private model_manager() { }
+    public static bool modelCheck(string path)
+    {
+        try
+        {
+            CubismModel3Json.LoadAtPath(path, BuiltinLoadAssetAtPath);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
 
     public static object BuiltinLoadAssetAtPath(Type assetType, string path)
     {
@@ -38,7 +48,7 @@ public class model_manager : MonoBehaviour
     public static void AddModel(string path) {
         try
         {
-            models[GameManager.userId] = CubismModel3Json.LoadAtPath(path);
+            models[GameManager.userId] = CubismModel3Json.LoadAtPath(path, BuiltinLoadAssetAtPath);
             if (objects[GameManager.userId])
             {
                 try
