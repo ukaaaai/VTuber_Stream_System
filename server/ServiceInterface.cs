a96f53e9c6c6220e136b4cd5962e79c4f79bd52b
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2022-08-15 21:41:54 +0900

    clean

--- server/ServiceInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿using MagicOnion;
-
-namespace server.Definition
-{
-    public interface ServiceInterface : IService<ServiceInterface>
-    {
-        UnaryResult<int> SumAsync(int x, int y);
-    }
-}
