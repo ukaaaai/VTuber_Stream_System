90448c497f1d4645434ca7c1bceb8f9ed5b49e2d
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-02-25 01:24:21 +0900

    update: load Live2D model

---
+++ Assets/scripts/Live2Dmodel/ModelManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using Live2D.Cubism.Core;
+using Live2D.Cubism.Framework.Json;
+using Live2D.Cubism.Rendering;
+using UnityEngine;
+
+namespace Live2Dmodel
+{
+    public class ModelManager : MonoBehaviour
+    {
+        private string _modelPath;
+        private CubismModel _model;
+
+        public void SetPath(string path)
+        {
+            _modelPath = path;
+        }
+        
+        public void LoadModel()
+        {
+            Destroy(_model);
+            var model3Json = CubismModel3Json.LoadAtPath(_modelPath, LoadAsset);
+            
+            _model = model3Json.ToModel();
+            
+            var parent = GameObject.Find("Canvas");
+            Transform transform1;
+            (transform1 = _model.transform).SetParent(parent.transform);
+            transform1.localPosition = Vector3.zero;
+            transform1.localScale = Vector3.one * Screen.height / 2.2f;
+            
+            var controller = _model.gameObject.GetComponent<CubismRenderController>();
+            controller.SortingOrder = 1;
+            controller.SortingMode = CubismSortingMode.BackToFrontOrder;
+        }
+
+        private static object LoadAsset(Type assetType, string absolutePath)
+        {
+            if (assetType == typeof(byte[]))
+            {
+                return File.ReadAllBytes(absolutePath);
+            }
+
+            if (assetType == typeof(string))
+            {
+                return File.ReadAllText(absolutePath);
+            }
+
+            if (assetType != typeof(Texture2D)) throw new NotSupportedException();
+            var texture = new Texture2D(1, 1);
+
+
+            texture.LoadImage(File.ReadAllBytes(absolutePath));
+
+
+            return texture;
+        }
+    }
+}
