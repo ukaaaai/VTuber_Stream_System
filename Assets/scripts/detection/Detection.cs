f7ad9aba3bea470466c7811a339d6b3af9d18d1a
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-05-23 13:24:05 +0900

    update: static class

--- Assets/scripts/detection/Detection.cs
+++ Assets/scripts/detection/Detection.cs
@@ -8,62 +8,47 @@ using UnityEngine;
 
 namespace detection
 {
-    public sealed class Detection
+    public static class Detection
     {
-        private static FrontalFaceDetector _faceDetector;
-        private static ShapePredictor _shapePredictor;
+        private static readonly FrontalFaceDetector FaceDetector;
+        private static readonly ShapePredictor ShapePredictor;
 
-        private static Detection _instance;
-        public static Detection Instance
+        static Detection()
         {
-            get
-            {
-                return _instance ??= new Detection();
-            }
-        }
-
-        public static void Init()
-        {
-            _instance = null;
-            _instance = new Detection();
-        }
-
-        private Detection()
-        {
-            _faceDetector = Dlib.GetFrontalFaceDetector();
+            FaceDetector = Dlib.GetFrontalFaceDetector();
             using var fs = File.OpenRead(Application.dataPath + "/StreamingAssets/shape_predictor_68_face_landmarks.dat");
             var bytes = new byte[fs.Length];
             // ReSharper disable once MustUseReturnValue
             fs.Read(bytes, 0, bytes.Length);
-            _shapePredictor = ShapePredictor.Deserialize(bytes);
+            ShapePredictor = ShapePredictor.Deserialize(bytes);
         }
 
-        public Param? Detect(Mat mat)
+        public static void Init(){}
+
+        public static Param? Detect(in Mat mat)
         {
-            var elemSize = mat.ElemSize();
+            var steps = mat.ElemSize() * CameraManager.Width;
             
-            var array = new byte[CameraManager.Width * CameraManager.Height * elemSize];
+            var array = new byte[CameraManager.Height * steps];
             Marshal.Copy(mat.Data, array, 0, array.Length);
             
-            var image = Dlib.LoadImageData<RgbPixel>(
+            using var image = Dlib.LoadImageData<RgbPixel>(
                 array,
                 (uint)CameraManager.Height, 
                 (uint)CameraManager.Width,
-                (uint)(CameraManager.Width * elemSize));
+                (uint)steps);
             
-            var faces = _faceDetector.Operator(image);
-
+            var faces = FaceDetector.Operator(image);
             if (faces.Length is 0) return null;
 
-            var shapes = _shapePredictor.Detect(image, faces[0]);
-            
+            var shapes = ShapePredictor.Detect(image, faces[0]);
             var points = new Complex[68];
             Parallel.For(0, 68, i =>
             {
                 var point = shapes.GetPart((uint)i);
                 points[i] = new Complex(point.X, point.Y);
             });
-            return CoordinateParser.Parse(points, image, mat);
+            return CoordinateParser.Parse(points, (CameraManager.Height, CameraManager.Width), mat);
         }
     }
 }
