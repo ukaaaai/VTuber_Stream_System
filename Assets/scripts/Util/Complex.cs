03006179417874c010e630ead4cb1831806547b6
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-05-27 16:58:03 +0900

    update: add phase, compare method and so on.

--- Assets/scripts/Util/Complex.cs
+++ Assets/scripts/Util/Complex.cs
@@ -1,6 +1,8 @@
-﻿namespace Util
+﻿using System;
+
+namespace Util
 {
-    public readonly struct Complex
+    public readonly struct Complex: IEquatable<Complex>, IComparable<Complex>, IFormattable
     {
         public readonly float Real;
         public readonly float Imaginary;
@@ -25,65 +27,96 @@
 
         public float Magnitude => 1 / Invsqrt();
         public float SqrMagnitude => Real * Real + Imaginary * Imaginary;
+        public float Phase => MathF.Atan2(Imaginary, Real);
         
-        public static Complex operator +(Complex a, Complex b)
+        public static Complex operator +(in Complex a, in Complex b)
         {
             return new Complex(a.Real + b.Real, a.Imaginary + b.Imaginary);
         }
         
-        public static Complex operator -(Complex a, Complex b)
+        public static Complex operator -(in Complex a, in Complex b)
         {
             return new Complex(a.Real - b.Real, a.Imaginary - b.Imaginary);
         }
         
-        public static Complex operator *(Complex a, Complex b)
+        public static Complex operator *(in Complex a, in Complex b)
         {
             return new Complex(a.Real * b.Real - a.Imaginary * b.Imaginary, a.Real * b.Imaginary + a.Imaginary * b.Real);
         }
         
-        public static Complex operator *(Complex a, float b)
+        public static Complex operator *(in Complex a, in float b)
         {
             return new Complex(a.Real * b, a.Imaginary * b);
         }
         
-        public static Complex operator *(float a, Complex b)
+        public static Complex operator *(in float a, in Complex b)
         {
             return new Complex(a * b.Real, a * b.Imaginary);
         }
         
-        public static Complex operator /(Complex a, float b)
+        public static Complex operator /(in Complex a, in float b)
         {
             return new Complex(a.Real / b, a.Imaginary / b);
         }
         
-        public static Complex operator /(float a, Complex b)
+        public static Complex operator /(in float a, in Complex b)
         {
             return new Complex(a * b.Real, a * b.Imaginary);
         }
         
-        public static Complex operator /(Complex a, Complex b)
+        public static Complex operator /(in Complex a, in Complex b)
         {
             return new Complex(a.Real * b.Real + a.Imaginary * b.Imaginary, a.Imaginary * b.Real - a.Real * b.Imaginary) / (b.Real * b.Real + b.Imaginary * b.Imaginary);
         }
         
-        public static Complex operator -(Complex a)
+        public static Complex operator -(in Complex a)
         {
             return new Complex(-a.Real, -a.Imaginary);
         }
         
-        public static Complex operator ~(Complex a)
+        public static Complex operator ~(in Complex a)
         {
             return new Complex(a.Real, -a.Imaginary);
         }
         
-        public static Complex operator ++(Complex a)
+        public static Complex operator ++(in Complex a)
         {
             return new Complex(a.Real + 1, a.Imaginary);
         }
         
-        public static Complex operator --(Complex a)
+        public static Complex operator --(in Complex a)
         {
             return new Complex(a.Real - 1, a.Imaginary);
+        }
+
+        public bool Equals(Complex other)
+        {
+            return Real.Equals(other.Real) && Imaginary.Equals(other.Imaginary);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Complex other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Real, Imaginary);
+        }
+        
+        public override string ToString(){
+            return $"{Real}+{Imaginary}i";
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return ToString();
+        }
+
+        public int CompareTo(Complex other)
+        {
+            var realComparison = Real.CompareTo(other.Real);
+            return realComparison != 0 ? realComparison : Imaginary.CompareTo(other.Imaginary);
         }
     }
 }
